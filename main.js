20ed2d16313d21ad87935060f9624b3a1275ce65
Paula <paula.deville@posteo.net>
2021-02-04 16:31:16 +0100

    design update

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobile_nav = document.querySelector('#mobile-nav');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('is-active');
+        mobile_nav.classList.toggle('is-active');
+    })
+}
