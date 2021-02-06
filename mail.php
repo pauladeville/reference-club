383ec1065c109023f57b1bd54e874f07ce5cd9ec
Paula <paula.deville@posteo.net>
2021-02-06 10:05:59 +0100

    test php

--- mail.php
+++ mail.php
@@ -1,23 +1,22 @@
-<?php
-    $destinataire = 'pauline.desmarets@posteo.net';
-    // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
-    $expediteur = $_POST['mail'];
-     
-    $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
-    $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
-    $headers .= 'To: '.$destinataire."\n"; // Mail de reponse
-    $headers .= 'From: <'.$expediteur.'>'."\n"; // Expediteur
-    
-    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> Message de '.$_POST['name'].' : <br>
-                    '.$_POST['message'].'</div>';
-     
-    if(mail($destinataire, $message, $headers))
-    {
-        echo '<script languag="javascript" >alert("Votre message a bien été envoyé ");</script>';
-    }
-    else // Non envoyé
-    {
-        echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé");</script>';
-    }
-    header('Location: https://pauladeville.github.io/reference-club/index.html#contact.php');
+<?php 
+
+$msg = "Nom:\t$name\n";
+$msg .= "E-Mail:\t$mail\n";
+$msg .= "Message:\t$message\n\n";
+
+$recipient = "pauline.desmarets@posteo.net";
+$subject = "Nouveau message sur REFERENCE CLUB";
+
+$mailheaders = "From: Test<> \n";
+$mailheaders .= "Reply-To: $email\n\n";
+
+mail($recipient, $subject, $msg, $mailheaders);
+
+echo "<HTML><HEAD>";
+echo "<TITLE>Votre message a été envoyé !</TITLE></HEAD><BODY>";
+echo "<H1 align=center>Merci, $nom </H1>";
+echo "<P align=center>";
+echo "Je vous recontacte très vite. Bonne journée !</P>";
+echo "</BODY></HTML>";
+
 ?>
