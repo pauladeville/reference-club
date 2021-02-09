7bf48ffb80031e154c5403a61f0704d798e6bc2b
Paula <paula.deville@posteo.net>
2021-02-09 16:36:52 +0100

    fixed bugs

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
         $header="MIME-Version: 1.0\r\n";
-        $header.='From:"Reference Club"<pauline.desmarets@posteo.net>'."\n";
+        $header.='From:"REFERENCE CLUB"<pauline.desmarets@posteo.net>'."\n";
         $header.='Content-Type:text/html; charset="uft-8"'."\n";
         $header.='Content-Transfer-Encoding: 8bit';
         $message='
